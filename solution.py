1b16d204f4828ab3fcafcccfe294370e1a88b50b
orangeparrotcarrot <72738134+orangeparrotcarrot@users.noreply.github.com>
2021-03-11 15:37:07 +0000

    data files

--- solution.py
+++ solution.py
@@ -1,3 +1,11 @@
-filename = ''
-data = open(filename+'.txt','r')
-info = data.readline().split
+filename = 'data_scenarios_a_example.in'
+data = open(filename,'r')
+W,H = (int(x) for x in data.readline().split())
+N,M,R = (int(x) for x in data.readline().split())
+buildings = []
+for i in range(N):
+    buildings.append([int(x) for x in data.readline().split()])
+antennas = []
+for i in range(M):
+    antennas.append([int(x) for x in data.readline().split()])
+print(W,H,N,M,R,buildings,antennas)
