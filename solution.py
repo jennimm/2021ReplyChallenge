306c69984ee788133ce2d31ea8ee55b02d7bc5c9
Jenni M <jennifermoulder17@gmail.com>
2021-03-11 15:49:41 +0000

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,3 +9,13 @@ antennas = []
 for i in range(M):
     antennas.append([int(x) for x in data.readline().split()])
 print(W,H,N,M,R,buildings,antennas)
+
+antennasRank = []
+for x in range(len(antennas)):
+    score = (antennas[x][0] + antennas[x][1]) // 2
+    antennasRank.append([x, score])
+antennasRank.sort(key = lambda x:x[1], reverse=True)
+
+print(antennasRank)
+
+    
